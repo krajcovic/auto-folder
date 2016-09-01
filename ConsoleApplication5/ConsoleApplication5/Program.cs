b38abdbdbb5ce89d4c9649f0d8466f9b2725b048
Dusan Krajcovic <dusan.krajcovic@gmail.com>
2016-09-01 13:38:07 +0200

    Ukazka

--- ConsoleApplication5/ConsoleApplication5/Program.cs
+++ ConsoleApplication5/ConsoleApplication5/Program.cs
@@ -10,33 +10,57 @@ namespace ConsoleApplication5
     {
         static void Main(string[] args)
         {
-            Slovník slovník = new Slovník();         //tohle mam jenom vycleneny prostor pro nejakou 
-                                                     //funkci na zjisteni jmena (je v tom textu hned za slovem "Table")
-            string text = "table";
-            var JmenoStolu = "";                     //tohle by se pozdeji melo vyplnit 
-            var a = "Jmeno stolu";                   //tohle se vypise pred 
+            if(args.Length != 1)
+            {
+                Console.WriteLine("Spatny pocet parametru, zadej nazev souboru, ktery chces prohledavat");
+                return;
+            }
+
+            Parser parser = new Parser();                                     
+                                                     
+            string keyWord = "table";
+            const string STR_NAME_OF_TABLE = "Jmeno stolu";                   // Tohle je konstanta, nikdy se nezmeni, tak ji taky tak deklaruj
 
             // Ctenni souboru line by line
-            string[] lines = System.IO.File.ReadAllLines("c:\\text.txt");
-            foreach (string line in lines)
+            try
             {
-                if (text != "table")                 //nenajde hledane slovo "table"
-                {                                    //vypise chybu "chyba"
-                    Console.WriteLine("chyba");
-                    break;
-                }
-                else
+                string[] lines = System.IO.File.ReadAllLines(args[0]);
+                foreach (string line in lines)
                 {
-                    Console.WriteLine(a + JmenoStolu);//tady to melo vipsat var a "Table Name" + jmeno toho stolu
+                    if (line.Contains(keyWord)) // Zjisti jestli line obsahuje klicove slovo(table)
+                    {
+                        // TODO: Tady si najdu teda jmeno stolu, protoze uz vim 
+                        var nameOfTable = Parser.getNameOfTable(line, keyWord);
+                        Console.WriteLine(STR_NAME_OF_TABLE + " : " +  nameOfTable); //tady to melo vipsat var a "Table Name" + jmeno toho stolu
+
+                    }
+                    else
+                    {
+                        // Jestlize ho nenajde, tak 
+                        Console.WriteLine("Tohle rozhodne neni hledany radek: " + line);
+                    }
                 }
-            } while (text != null) ;
+            } catch (Exception e)
+            {
+                // Co budes delat, kdyz ten soubor neexistuje?
+                Console.WriteLine(e.Message);
+            }
 
-
+            Console.WriteLine("Press ENTER for finish.");
             Console.ReadLine();
         }
     }
-    class Slovník
+
+    /*
+    Jestli te jeste jednou uvidim, ze si pojmenoval jakykoliv objekt s ceskou diakritikou (hacky, carky) tak
+    normalne dojedu a nakopu te, tam kam slunce nesviti.
+    */
+    class Parser
     {
         // tady mel a byt funkce ktera dosadi do "string jmeno" nazev stolu.
+        internal static string getNameOfTable(string line, string keyWord)
+        {
+            return line.Substring(line.IndexOf(keyWord));
+        }
     }
 }
